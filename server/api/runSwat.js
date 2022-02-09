e9643886a7806b66bde4f340810b2bb40a8151ee
Peter Butcher <pbutcher93@gmail.com>
2022-02-09 14:56:56 +0000

    Fix

--- server/api/runSwat.js
+++ server/api/runSwat.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const { config } = require("./config");
+
 module.exports = (req, res) => {
   let scenario = req.query.scenario;
   // console.log(scenario);
@@ -14,3 +17,42 @@ module.exports = (req, res) => {
     });
   }
 };
+
+// API METHOD: runSwat
+// Run SWAT
+function runSWAT(res, scenario) {
+  const process = require("child_process").spawn(
+    path.resolve(
+      __dirname,
+      `${config().swat_scenarios}${scenario}/TxtInOut/${config().swat_exe}`
+    ),
+    [],
+    {
+      cwd: `${config().swat_scenarios_root}${scenario}/TxtInOut/`,
+      maxBuffer: 1024 * 1024 * 1024,
+    }
+  );
+
+  // Data to the screen while model is executing successfully
+  process.stdout.on("data", (data) => {
+    console.log(`${data}`);
+  });
+
+  // Log error message
+  process.stderr.on("data", (data) => {
+    console.log(`${data}`);
+    res.send({
+      code: 0,
+      message: `Error running swat for scenario: ${scenario}`,
+    });
+  });
+
+  // Perform any tasks when done if needed...
+  process.on("close", (code) => {
+    console.log("done");
+    res.send({
+      code: 1,
+      message: `Swat ran successfully for scenario: ${scenario}`,
+    });
+  });
+}
